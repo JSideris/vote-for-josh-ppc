0b34079bea509e51b92deaff7e238994d32e41d5
jsideris <josh.sideris@gmail.com>
2025-04-16 17:36:00 -0400

    Update broken btn.

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -44,8 +44,8 @@ const Hero: React.FC = () => {
 					>
 						Donate Now
 					</button> */}
-					<a href="https://donate.stripe.com/5kAcNGb0K2027vyfYY" target='_blank' className="btn btn-secondary" onClick={handleLinkClick}>
-						Learn More
+					<a href="https://donate.stripe.com/5kAcNGb0K2027vyfYY" target='_blank' className="btn btn-secondary">
+						Donate Now
 					</a>
 					<a href="#platform" className="btn btn-primary" onClick={handleLinkClick}>
 						Learn More
